967ab9123b8fae827c9e8eab3f86fd28b2b8a9e6
Ken <klquicksall@gmail.com>
2012-07-11 18:52:33 -0500

    .gitignore is now working

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 2 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Wed Jul 11 16:51:58 CDT 2012"
-#define LINUX_COMPILE_BY "ken"
-#define LINUX_COMPILE_HOST "klquicksall"
-#define LINUX_COMPILER "gcc version 4.5.2 (Sourcery G++ Lite 2011.03-42) "
---
+++ make3
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+
+make ARCH=arm CROSS_COMPILE=/home/ken/android/gcc-arm-none-eabi-4_6-2012q2/bin/arm-none-eabi- EXTRA_AFLAGS=-mfpu=neon
