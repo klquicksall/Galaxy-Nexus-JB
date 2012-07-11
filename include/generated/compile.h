cfe36d2407eb8b3322cd9a26215b7085fd5bd66c
Ken <klquicksall@gmail.com>
2012-07-11 15:58:30 -0500

    First Commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Jul 11 01:26:30 CDT 2012"
+#define LINUX_COMPILE_BY "ken"
+#define LINUX_COMPILE_HOST "klquicksall"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-188) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.0.31-BANNED-ga72596f"
